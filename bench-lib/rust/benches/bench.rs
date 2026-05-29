276c1ee8c5978ee211fcf56c83670d172f139b74
Patrick Amrein <amrein@ubique.ch>
2026-05-29 11:08:34 +0200

    add old bench

--- bench-lib/rust/benches/bench.rs
+++ bench-lib/rust/benches/bench.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use next_gen_signatures::{Engine, BASE64_URL_SAFE_NO_PAD};
-use signatures_bench::zkp_verify_native;
+use signatures_bench::{prepare, zkp_issue, zkp_present, zkp_verify, zkp_verify_native};
 use zkp_util::ecdsa_pops::halo2curves::ff::derive::byteorder::{BigEndian, WriteBytesExt};
 
 use std::{
@@ -62,5 +62,33 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_benchmark_old(c: &mut Criterion) {
+    let prep = prepare();
+
+    let vc = zkp_issue(&prep.issuer, &prep.device_binding);
+    let start = Instant::now();
+    let presentation = zkp_present(
+        vc,
+        &prep.issuer.public_key,
+        &prep.proving_keys,
+        prep.db_public_key,
+        prep.message.clone(),
+        prep.message_signature,
+    );
+    let end = Instant::now();
+    println!("{}", (end - start).as_millis());
+
+    c.bench_function("verification", |b| {
+        b.iter(|| {
+            black_box(zkp_verify(
+                presentation.clone(),
+                &prep.issuer.public_key,
+                &prep.verifying_keys,
+                prep.message.clone(),
+            ))
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark, criterion_benchmark_old);
 criterion_main!(benches);
